1b286f06e8bbf789145f824899936e3ec29f6689
Antony <9141998@gmail.com>
2017-10-10 16:32:24 +0300

    Урок 12. Создаём каталог товара. Вывел все товары на страницу каталога. Блок div одел в цикл foreach.
    <?php foreach ($goods as $good): ?>
        <div class="shopUnit">
    -/ Содержимое /-
        </div>
    <?php endforeach; ?>

--- templates/shop.php
+++ templates/shop.php
@@ -2,21 +2,23 @@
                 Каталог товаров
 </h1>
 
-<div class="shopUnit">
-    <img src="http://placehold.it/308x231" />
+<?php foreach ($goods as $good): ?>
+    <div class="shopUnit">
+        <img src="http://placehold.it/308x231" />
 
-    <div class="shopUnitName">
-        Название продукта
+        <div class="shopUnitName">
+            Название продукта
+        </div>
+        <div class="shopUnitShortDesc">
+            Здесь будет немного текста описывающего продукт.
+            Здесь будет немного текста описывающего продукт.
+            Здесь будет немного текста описывающего продукт.
+        </div>
+        <div class="shopUnitPrice">
+            Цена: 0000 $
+        </div>
+        <a href="#" class="shopUnitMore">
+            Подробнее
+        </a>
     </div>
-    <div class="shopUnitShortDesc">
-        Здесь будет немного текста описывающего продукт.
-        Здесь будет немного текста описывающего продукт.
-        Здесь будет немного текста описывающего продукт.
-    </div>
-    <div class="shopUnitPrice">
-        Цена: 0000 $
-    </div>
-    <a href="#" class="shopUnitMore">
-        Подробнее
-    </a>
-</div>
+<?php endforeach; ?>
